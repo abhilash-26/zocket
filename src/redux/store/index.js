a5a29e2173ed4ae1d50020c6fba88da760b24ba5
abhilash-26 <krabhilash226@gmail.com>
2021-09-06 14:49:02 +0530

    done

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,22 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../features/user";
-import profilePopupReducer from "../features/propfilePopup";
-import signInPopupReducer from "../features/signInPopup";
-import allJobs from "../features/allJobs";
-import savedJob from "../features/getSavedJobs";
-import searchedJob from "../features/searchedJob";
-import searchLoading from "../features/searchLoading";
 
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-    profilePopup: profilePopupReducer,
-    signInPopup: signInPopupReducer,
-    allJobs: allJobs,
-    savedJob: savedJob,
-    searchedJob: searchedJob,
-    searchLoading: searchLoading,
-  },
+  reducer: {},
 });
 
 export default store;
